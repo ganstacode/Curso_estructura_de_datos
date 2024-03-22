8a6cff2215e98247070a0afc1478fc863db0bcbc
ganstacode <202311447@ives.edu.mx>
2024-03-22 01:54:23 -0600

    Colas

---
+++ colas/main.java
@@ -0,0 +1,19 @@
+package colas;
+
+public class main {
+
+    public static void main (String [] args) {
+        
+    colas cola = new colas();
+
+
+    cola.agregarCola(1,"Julian");
+    cola.agregarCola(2,"julia");
+    cola.agregarCola(3,"roco");
+    cola.agregarCola(4,"lupita");
+    
+    cola.saberPedido();
+
+    }
+    
+}
